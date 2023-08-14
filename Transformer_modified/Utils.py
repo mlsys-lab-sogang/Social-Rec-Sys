38348e32e8578d0e2245ba8f358ea9b74a8291cb
CharlieKim <skdbsxir@naver.com>
2023-08-14 18:23:01 +0900

    Utils: move function to utils

--- Transformer_modified/Utils.py
+++ Transformer_modified/Utils.py
@@ -1,12 +1,14 @@
-"""
-Original code: fairseq
-    https://github.com/facebookresearch/fairseq/tree/main/fairseq
-"""
+import math
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
 class DropoutModule(nn.Module):
+    """
+    Original code: fairseq
+        https://github.com/facebookresearch/fairseq/tree/main/fairseq
+    """
     def __init__(self, p, module_name=None) -> None:
         super().__init__()
         self.p = p
@@ -18,3 +20,14 @@ class DropoutModule(nn.Module):
             return F.dropout(x, p=self.p, training=True, inplace=inplace)
         else:
             return x
+
+def init_params(module, n_layers):
+    """
+    Initialize parameters following module type.
+    """
+    if isinstance(module, nn.Linear):
+        module.weight.data.normal__(mean=0.0, std=0.02 / math.sqrt(n_layers))
+        if module.bias is not None:
+            module.bias.data.zero_()
+    if isinstance(module, nn.Embedding):
+        module.weight.data.normal_(mean=0.0, std=0.02)
