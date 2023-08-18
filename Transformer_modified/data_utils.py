bedcbe3f9a96bb7f3c6400f87100550d455d06db
CharlieKim <skdbsxir@naver.com>
2023-08-18 18:35:28 +0900

    data_utils: add degree info to result dictionary But when processing all nodes, random walk sequence seems to be duplicated for some user nodes. Need to check reason (maybe in file_next_social_node() ?)

--- Transformer_modified/data_utils.py
+++ Transformer_modified/data_utils.py
@@ -55,14 +55,15 @@ def mat_to_csv(data_path:str):
     trust_df.to_csv(data_path + '/trustnetwork.csv', index=False)
 
 
-def generate_user_degree_table(data_path:str):
+def generate_user_degree_table(data_path:str) -> pd.DataFrame:
     """
-    Generate degree table from social graph(trustnetwork).
+    Generate & return degree table from social graph(trustnetwork).
     """
     # processed file check
     if 'social_degree.csv' in os.listdir(data_path):
         print("Processed .csv file already exists...")
-        return 0
+        degree_df = pd.read_csv(data_path + '/social_degree.csv', index_col=[])
+        return degree_df
 
     # user-user network
         # Ciao: 7317 users
@@ -76,11 +77,19 @@ def generate_user_degree_table(data_path:str):
 
     degree_df.to_csv(data_path + '/social_degree.csv', index=False)
 
+    return degree_df
 
-def generate_social_random_walk_sequence(data_path:str, num_nodes:int=10, walk_length:int=5, save_flag=False, all_node=False, seed=False) -> dict:
+
+def generate_social_random_walk_sequence(data_path:str, num_nodes:int=10, walk_length:int=5, save_flag=False, all_node=False, seed=False) -> list:
     """
     Generate random walk sequence from social graph(trustnetwork).
-    
+    Return:
+        List containing dictionaries,\n 
+        ex) [
+            {user_id: [[user, ..., user], [degree, ..., degree]]}, 
+            {user_id: [[user, ..., user], [degree, ..., degree]]},
+            ...
+            ]
     Args:
         data_path: path to data
         num_nodes: number of nodes to generate random walk sequence
@@ -93,8 +102,9 @@ def generate_social_random_walk_sequence(data_path:str, num_nodes:int=10, walk_l
     dataframe = pd.read_csv(trust_file, index_col=[])
 
     social_graph = nx.from_pandas_edgelist(dataframe, source='user_id_1', target='user_id_2')
+    degree_table = generate_user_degree_table(data_path=data_path)
 
-    path_dict = {}
+    all_path_list = []
 
     if all_node:
         num_nodes = len(social_graph.nodes())
@@ -102,11 +112,12 @@ def generate_social_random_walk_sequence(data_path:str, num_nodes:int=10, walk_l
     if seed:
         np.random.seed(62)
 
-    # select target(anchor) nodes randomly.
-    anchor_nodes = np.random.choice(social_graph.nodes(), size=num_nodes)
+    # select target(anchor) nodes randomly. (without replacement)
+    anchor_nodes = np.random.choice(social_graph.nodes(), size=num_nodes, replace=False)
     
     # At first, there is no previous node, so set it to None.
     for nodes in tqdm(anchor_nodes, desc="Generating random walk sequence..."):
+        path_dict = {}
         path_dict[nodes] = [nodes]
         for _ in range(walk_length - 1):
             # Move to one of connected node randomly.
@@ -125,18 +136,44 @@ def generate_social_random_walk_sequence(data_path:str, num_nodes:int=10, walk_l
         
         # Pop 1st element of list(since it is anchor node).
         del path_dict[nodes][0]
-    
+        
+        # # Get each user's degree information from degree table.
+        degree_list = []
+        for node_list in path_dict.values():
+            for node in node_list:
+                if node != 0:
+                    degree = degree_table['degree'].loc[degree_table['user_id'] == node].values[0]
+                else:
+                    # If node is 0 (zero-padded value), returns 0.
+                    degree = 0
+                degree_list.append(degree)
+        
+        # Add degree information to path_dict.
+        path_dict = {key: [value, degree_list] for key, value in path_dict.items()}
+        all_path_list.append(path_dict)
+
     if save_flag:
         # save result to .csv
-        path = data_path + '/' + f"social_user_{num_nodes}_rw_length_{walk_length}_seed_.csv"
-        columns = ['user_id', 'random_walk_seq']
-        result_df = pd.DataFrame(path_dict.items(), columns=columns)
+        path = data_path + '/' + f"social_user_{num_nodes}_rw_length_{walk_length}_fixed_seed_{seed}.csv"
+
+        keys, walks, degrees = [], [], []
+        for paths in all_path_list:
+            for key, value in paths.items():
+                keys.append(key)
+                walks.append(value[0])
+                degrees.append(value[1])
+
+        result_df = pd.DataFrame({
+            'user_id':keys,
+            'random_walk_seq':walks,
+            'degree':degrees
+        })
         result_df.sort_values(by=['user_id'], inplace=True)
         result_df.reset_index(drop=True, inplace=True)
         
         result_df.to_csv(path, index=False)
         
-    return path_dict
+    return all_path_list
 
 
 def find_next_social_node(graph:nx.Graph(), previous_node, current_node, RETURN_PARAMS, seed=False):
@@ -179,7 +216,10 @@ def find_next_social_node(graph:nx.Graph(), previous_node, current_node, RETURN_
     return selected_node
 
 # For checking
-sequences = generate_social_random_walk_sequence(data_path, num_nodes=10, walk_length=20, save_flag=False, all_node=False, seed=False)
-for key, value in sequences.items():
-    print({f"{key} : {value}"})
-    print(f"\t => length: {len(value)}")
+sequence_list = generate_social_random_walk_sequence(data_path, num_nodes=10, walk_length=20, save_flag=True, all_node=True, seed=True)
+# for sequences in sequence_list:
+#     for key, value in sequences.items():
+#         # print({f"{key} : {value}"})
+#         print(key)
+#         print('\t', value[0])
+#         print('\t', value[1])
