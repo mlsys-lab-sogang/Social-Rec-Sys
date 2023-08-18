ba7a1635bd31f70be897d8f8ed24be08520ab67b
CharlieKim <skdbsxir@naver.com>
2023-08-18 16:18:29 +0900

    data_utils: add seed & file saving

--- Transformer_modified/data_utils.py
+++ Transformer_modified/data_utils.py
@@ -14,8 +14,7 @@ import numpy as np
 import pandas as pd
 import scipy.sparse as sp
 from scipy.io import loadmat
-
-# np.random.seed(62)
+from tqdm.auto import tqdm
 
 # arg(or else) passing to DATASET later
 DATASET = 'ciao'
@@ -78,7 +77,7 @@ def generate_user_degree_table(data_path:str):
     degree_df.to_csv(data_path + '/social_degree.csv', index=False)
 
 
-def generate_social_random_walk_sequence(data_path:str, num_nodes:int=10, walk_length:int=5) -> dict:
+def generate_social_random_walk_sequence(data_path:str, num_nodes:int=10, walk_length:int=5, save_flag=False, all_node=False, seed=False) -> dict:
     """
     Generate random walk sequence from social graph(trustnetwork).
     
@@ -86,6 +85,9 @@ def generate_social_random_walk_sequence(data_path:str, num_nodes:int=10, walk_l
         data_path: path to data
         num_nodes: number of nodes to generate random walk sequence
         walk_length: length of random walk
+        save_flag: save result locally (default=False)
+        all_node: get all node's random walk sequence (default=False)
+        seed: random seed, True or False (default=False)
     """
     trust_file = data_path + '/trustnetwork.csv'
     dataframe = pd.read_csv(trust_file, index_col=[])
@@ -94,16 +96,22 @@ def generate_social_random_walk_sequence(data_path:str, num_nodes:int=10, walk_l
 
     path_dict = {}
 
+    if all_node:
+        num_nodes = len(social_graph.nodes())
+    
+    if seed:
+        np.random.seed(62)
+
     # select target(anchor) nodes randomly.
     anchor_nodes = np.random.choice(social_graph.nodes(), size=num_nodes)
     
     # At first, there is no previous node, so set it to None.
-    for nodes in anchor_nodes:
+    for nodes in tqdm(anchor_nodes, desc="Generating random walk sequence..."):
         path_dict[nodes] = [nodes]
         for _ in range(walk_length - 1):
             # Move to one of connected node randomly.
             if path_dict[nodes][-1] == nodes:
-                next_node = find_next_social_node(graph=social_graph, previous_node=None, current_node=nodes, RETURN_PARAMS=0.0)
+                next_node = find_next_social_node(graph=social_graph, previous_node=None, current_node=nodes, RETURN_PARAMS=0.0, seed=seed)
                 path_dict[nodes].append(next_node)
 
             # If selected node was "edge node", there is no movable nodes, so pad it with 0(zero-padding).
@@ -112,16 +120,26 @@ def generate_social_random_walk_sequence(data_path:str, num_nodes:int=10, walk_l
 
             # Move to one of connected node randomly.
             else:
-                next_node = find_next_social_node(graph=social_graph, previous_node=path_dict[nodes][-2], current_node=path_dict[nodes][-1], RETURN_PARAMS=0.0)
+                next_node = find_next_social_node(graph=social_graph, previous_node=path_dict[nodes][-2], current_node=path_dict[nodes][-1], RETURN_PARAMS=0.0, seed=seed)
                 path_dict[nodes].append(next_node)
         
         # Pop 1st element of list(since it is anchor node).
         del path_dict[nodes][0]
-
+    
+    if save_flag:
+        # save result to .csv
+        path = data_path + '/' + f"social_user_{num_nodes}_rw_length_{walk_length}_seed_.csv"
+        columns = ['user_id', 'random_walk_seq']
+        result_df = pd.DataFrame(path_dict.items(), columns=columns)
+        result_df.sort_values(by=['user_id'], inplace=True)
+        result_df.reset_index(drop=True, inplace=True)
+        
+        result_df.to_csv(path, index=False)
+        
     return path_dict
 
 
-def find_next_social_node(graph:nx.Graph(), previous_node, current_node, RETURN_PARAMS):
+def find_next_social_node(graph:nx.Graph(), previous_node, current_node, RETURN_PARAMS, seed=False):
     """
     Find connected nodes, using transition probability. \n
         ###\n
@@ -129,6 +147,9 @@ def find_next_social_node(graph:nx.Graph(), previous_node, current_node, RETURN_
             (no re-visiting to previously visited node.)\n
         ###
     """
+    if seed:
+        np.random.seed(62)
+    
     select_prob = {}
 
     # Here, in social network we using, we don't have any edge weights.
@@ -158,7 +179,7 @@ def find_next_social_node(graph:nx.Graph(), previous_node, current_node, RETURN_
     return selected_node
 
 # For checking
-sequences = generate_social_random_walk_sequence(data_path, num_nodes=10, walk_length=20)
+sequences = generate_social_random_walk_sequence(data_path, num_nodes=10, walk_length=20, save_flag=False, all_node=False, seed=False)
 for key, value in sequences.items():
     print({f"{key} : {value}"})
     print(f"\t => length: {len(value)}")
