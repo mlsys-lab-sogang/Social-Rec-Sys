a8c54cbda91ab4aa168f353e926694069e840602
CharlieKim <skdbsxir@naver.com>
2023-08-24 16:34:17 +0900

    dataset: 1st construction complete But item_seq and item_rating returns little different as desired, fixing is under going.

--- Transformer_modified/dataset.py
+++ Transformer_modified/dataset.py
@@ -1,7 +1,7 @@
 import os
-import networkx as nx
 import numpy as np
 import pandas as pd
+from ast import literal_eval
 
 import torch
 from torch.utils.data import Dataset, DataLoader
@@ -11,64 +11,85 @@ import data_utils as utils
 class MyDataset(Dataset):
     """
     Process & make dataset for DataLoader.
-        __init__():     data load & process
+        __init__():     data load & convert to Tensor
         __getitem__():  data indexing (e.g. dataset[0])
         __len__():      length of dataset (e.g. len(dataset))
+        load_data():    load data and preprocess data into ndarray
     """
     def __init__(self, dataset:str):
         """
         Args:
             dataset: raw dataset name (ciao or epinions)
         """
-        self.root_dir = os.getcwd() + '/dataset/'
-        self.data_path = self.root_dir + dataset        # /dataset/ciao || /dataset/epinions
+        self.data_path = os.getcwd() + '/dataset/' + dataset
 
-        # generate random walk sequence
-            # If `seed=True`, duplicated nodes will appear. (since seed is fixed for np.random.choice())
-            # used for debugging & building
-        random_walk_seq = utils.generate_social_random_walk_sequence(
-            data_path = self.data_path,
-            num_nodes = 10,
-            walk_length = 20,
-            seed = True,
-            # all_node=True
+        user_sequences, user_degree, item_sequences, item_rating = self.load_data()
+
+        self.user_sequences = torch.FloatTensor(user_sequences)     # (num_user, walk_length)
+        self.user_degree = torch.FloatTensor(user_degree)           # (num_user, walk_length)
+        self.item_sequences = torch.FloatTensor(item_sequences)     # (num_user, item_length)
+        self.item_rating = torch.FloatTensor(item_rating)           # (num_user, item_length)
+
+        assert len(self.user_sequences) == len(self.user_degree) == len(self.item_sequences) == len(self.item_rating), (
+            f"All data should have same length: {len(self.user_sequences)}, {len(self.user_degree)}, {len(self.item_sequences)}, {len(self.item_rating)}"
         )
 
-        # generate item interaction information 
-            # number of interacted items of users(ciao): max 10,671, min 28, mean 260.4, std: 488.3
-        # social_user_interact_items = utils.find_social_user_interacted_items(self.data_path, random_walk_seq)     # users in random walk sequence
-        input_user_interact_items = utils.find_selected_user_interacted_items(self.data_path, random_walk_seq)    # users in random walk's start
+    def __len__(self):
+        return len(self.user_sequences)
+    
+    def __getitem__(self, idx):
+        user_seq = self.user_sequences[idx]
+        user_deg = self.user_degree[idx]
+        item_seq = self.item_sequences[idx]
+        item_rating = self.item_rating[idx]
+    
+        return user_seq, user_deg, item_seq, item_rating
 
-        # encoder's input user list
-            # shape: (num_nodes, )  
-        self.enc_input_user = np.array(list(input_user_interact_items.keys()))
+    def load_data(self):
+        """
+        Data load & preprocess to ndarray
+            # TODO: fix or handle later (using function, not reading .csv)
+        """
+        user_path = '/social_user_7317_rw_length_20_fixed_seed_False.csv'
+        item_df = utils.generate_interacted_items_table(data_path=self.data_path, item_length=4)
+
+        # load dataset & convert data type
+            # values are saved as 'str', convert into original type, 'list'.
+        user_df = pd.read_csv(self.data_path + user_path, index_col=[])
+        user_df['random_walk_seq'] = user_df.apply(lambda x: literal_eval(x['random_walk_seq']), axis=1)
+        user_df['degree'] = user_df.apply(lambda x: literal_eval(x['degree']), axis=1)
+
+        item_df['product_id'] = item_df.apply(lambda x: literal_eval(x['product_id']), axis=1)
+        item_df['rating'] = item_df.apply(lambda x: literal_eval(x['rating']), axis=1)
         
-        # generate(fetch) each input user's random walk sequence nodes.
-        # will be used in user-user relation encoding.
-            # shape: (num_nodes. walk_length)
-        user_stack = []
-        for walks in random_walk_seq:
-            for k, value in walks.items():
-                user_stack.append(value[0])
-        self.enc_input_friends = np.array(list(zip(*user_stack))).T
+        # Since each row's element is list, convert it.
+            # `user_sequences` & `user_degree` shape: (num_user, walk_length)
+        user_sequences = user_df['random_walk_seq'].to_numpy(dtype=object)
+        user_sequences = np.array([np.array(x) for x in user_sequences])
+        user_degree = user_df['degree'].to_numpy(dtype=object)
+        user_degree = np.array([np.array(x) for x in user_degree])
 
-        print(self.enc_input_user.shape)
-        print(self.enc_input_friends.shape)
+        # TODO: since there are users doesn't exist in social graph, but exists in user-item graph, drop those users in item table.
+        non_users = utils.find_non_existing_user_in_social_graph(data_path=self.data_path)
+        item_df = item_df[~item_df['user_id'].isin(non_users)]
+        item_df.reset_index(drop=True, inplace=True)
 
-        # TODO: Need to pre-define number of interacted items.
-            # If not, ndarray's length will be shortest length of interacted item.
-            # refer: https://stackoverflow.com/questions/53051560/stacking-numpy-arrays-of-different-length-using-padding
-        # generate(fetch) each input user's interacted items
-            # shape: [num_nodes, pre_defined_num_items]
-        item_stack = []
-        for key, value in input_user_interact_items.items():
-            item_stack.append(value[0])
-        for item in item_stack:
-            print(len(item))
-        # self.dec_input_items = np.array(list(zip(*item_stack))).T
-        # print(self.dec_input_items)
+        # Since item_df's row element's type is 'list'(not 'str' like user_df), just convert it into ndarray.
+        item_sequences = item_df['product_id'].to_numpy(dtype=object)
+        item_sequences = np.array([np.array(x) for x in item_sequences])
+        item_rating = item_df['rating'].to_numpy(dtype=object)
+        item_rating = np.array([np.array(x) for x in item_rating])
+
+        return user_sequences, user_degree, item_sequences, item_rating
 
 
-
-
-dataset = MyDataset(dataset='ciao')
+if __name__ == "__main__":
+    # for testing & debugging
+    dataset = MyDataset(dataset='ciao')
+    loader = DataLoader(dataset, batch_size=1, shuffle=True)
+    for data in loader:
+        print(data[0].shape)
+        print(data[1].shape)
+        print(data[2].shape)
+        print(data[3].shape)
+        quit()
