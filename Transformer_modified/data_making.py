be0791c629642effb39a35d624e9346ef9eaa706
CharlieKim <skdbsxir@naver.com>
2023-09-06 21:00:51 +0900

    data_making: added data processing script Should perform preprocessing by this script.

---
+++ Transformer_modified/data_making.py
@@ -0,0 +1,22 @@
+"""
+data_utils.py에 있는 함수들을 사용해
+.mat -> .csv -> degree table & interacted item table 등등 필요한 작업을 수행.
+
+TODO: 적절히 순차적으로 분기할 수 있도록 작성. 현재는 주석단위로 처리하고있음.
+"""
+import os
+
+import data_utils as utils
+
+
+data_path = os.getcwd() + '/dataset/' + 'ciao' 
+
+# utils.mat_to_csv(data_path)
+
+# utils.generate_user_degree_table(data_path)
+# utils.generate_item_degree_table(data_path)
+
+# utils.generate_interacted_items_table(data_path, all=True)
+# utils.generate_interacted_items_table(data_path, all=False, item_length=4)
+
+utils.generate_social_random_walk_sequence(data_path, num_nodes=10, walk_length=20, save_flag=True, all_node=True, seed=False)
