56d976ae38c65cf0d6ee4a01d04d6d89d8b2695d
CharlieKim <skdbsxir@naver.com>
2023-09-14 18:17:57 +0900

    transformer: add transformer model.

---
+++ Transformer_modified/models/transformer.py
@@ -0,0 +1,36 @@
+import torch
+import torch.nn as nn
+
+from models.encoder import Encoder
+from models.decoder import Decoder
+
+class Transformer(nn.Module):
+    def __init__(self, num_user, max_degree_user, num_item, max_degree_item, d_model, d_ffn, num_heads, dropout, num_layers):
+        super(Transformer, self).__init__()
+        
+        self.encoder = Encoder(
+            num_user=num_user,
+            max_degree=max_degree_user,
+            d_model=d_model,
+            d_ffn=d_ffn,
+            num_heads=num_heads,
+            dropout=dropout,
+            num_layers=num_layers
+        )
+
+        self.decoder = Decoder(
+            num_item=num_item,
+            max_degree=max_degree_item,
+            d_model=d_model,
+            d_ffn=d_ffn,
+            num_heads=num_heads,
+            dropout=dropout,
+            num_layers=num_layers
+        )
+    
+    def forward(self, batched_data):
+        enc_output, src_mask = self.encoder(batched_data)
+        # print(f"############### Enc end... {enc_output.shape} and {src_mask.shape} ###############")
+        output = self.decoder(batched_data, enc_output, src_mask)
+
+        return output
