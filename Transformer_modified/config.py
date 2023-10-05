8cca4b0f4ce87353105cbcdfbb18dc23af690ac0
CharlieKim <skdbsxir@naver.com>
2023-10-05 17:48:48 +0900

    config: fixed epinions configuration.

--- Transformer_modified/config.py
+++ Transformer_modified/config.py
@@ -11,9 +11,9 @@ ciao={
             "max_degree_user": 804,
             "num_item": 105114,
             "max_degree_item": 915,
-            "d_model": 128,          # MHA dim (Linear modules in Attention Network) & Embedding dim 
-            "d_ffn": 512,            # FFN dim
-            "num_heads": 4,
+            "d_model": 64,          # MHA dim (Linear modules in Attention Network) & Embedding dim 
+            "d_ffn": 256,            # FFN dim
+            "num_heads": 2,
             "dropout": 0.2,         # Inside FFN, decoder_layer & encoder_layer (applied after linear & attention)
             "num_layers_enc": 2,
             "num_layers_dec": 2
@@ -39,30 +39,28 @@ epinions={
              "test":38000,
          },
          "model":{
-             "embedding_dim":64, 
-             "transformer_dim":64, 
-             "transformer_hidden_dim":128, 
-             "head_dim":32, 
-             "num_head":2, 
-             "num_layers":2,
-             "vocab_size":512,
-             "encoder_seq_length":20, 
-             "decoder_seq_length":250, 
-             "dropout_prob":0.1,
-             "attention_dropout":0.1,
-             "num_classes": 5,
+            "num_user": 18098,
+            "max_degree_user": 2026,
+            "num_item": 261679,
+            "max_degree_item": 1440,
+            "d_model": 64,          # MHA dim (Linear modules in Attention Network) & Embedding dim 
+            "d_ffn": 256,            # FFN dim
+            "num_heads": 2,
+            "dropout": 0.2,         # Inside FFN, decoder_layer & encoder_layer (applied after linear & attention)
+            "num_layers_enc": 2,
+            "num_layers_dec": 2
          },
          "training":{
-             "batch_size":128,
-             "optimizer":"adam",
-             "learning_rate":0.0001,
-             "warmup":80, 
-             "lr_decay":"linear",
-             "weight_decay":0,
-             "eval_frequency":200, 
-             "num_epochs":10,
-             "num_eval_steps":100, 
-             "patience":10, 
+            "batch_size":128,        # total_train_step: 835 (1 epoch 당 `len(train_dataset) / batch_size`)
+            "optimizer":"adamw",
+            "learning_rate":0.001,
+            "warmup":80, 
+            "lr_decay":"linear",
+            "weight_decay":0,
+            "eval_frequency":400, 
+            "num_epochs":50,
+            "num_eval_steps":849,   # total_valid_sample / total_epoch
+            "patience":10, 
          },
      }
 
