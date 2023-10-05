aaeca9070afd6118cad66425d35a8affc556b8b0
CharlieKim <skdbsxir@naver.com>
2023-10-05 14:35:18 +0900

    added more ENV variables to .sh script

--- Transformer_modified/data_making.sh
+++ Transformer_modified/data_making.sh
@@ -1,51 +1,54 @@
 #!/usr/bin/env bash
 export DATASET="$1"
+export RANDOM_WALK_LEN="$2"
+export ITEM_SEQ_LEN="$3"
 
-python3 data_making.py --dataset "$DATASET" --first True --seed 42 --test_ratio 0.1 --random_walk_len 20 --item_seq_len 250
+python3 data_making.py --dataset "$DATASET" --first True --seed 42 --test_ratio 0.1 --random_walk_len "$RANDOM_WALK_LEN" --item_seq_len "$ITEM_SEQ_LEN"
 
 echo "seed 42 data making finished"
-sleep 1m
+sleep 15s
 
-python3 data_making.py --dataset "$DATASET" --seed 62 --test_ratio 0.1 --random_walk_len 20 --item_seq_len 250
+python3 data_making.py --dataset "$DATASET" --seed 62 --test_ratio 0.1 --random_walk_len "$RANDOM_WALK_LEN" --item_seq_len "$ITEM_SEQ_LEN"
 
 echo "seed 62 data making finished"
-sleep 1m
+sleep 15s
 
-python3 data_making.py --dataset "$DATASET" --seed 365 --test_ratio 0.1 --random_walk_len 20 --item_seq_len 250
+python3 data_making.py --dataset "$DATASET" --seed 365 --test_ratio 0.1 --random_walk_len "$RANDOM_WALK_LEN" --item_seq_len "$ITEM_SEQ_LEN"
 
 echo "seed 365 data making finished"
-sleep 1m
+sleep 15s
 
-python3 data_making.py --dataset "$DATASET" --seed 462 --test_ratio 0.1 --random_walk_len 20 --item_seq_len 250
+python3 data_making.py --dataset "$DATASET" --seed 462 --test_ratio 0.1 --random_walk_len "$RANDOM_WALK_LEN" --item_seq_len "$ITEM_SEQ_LEN"
 
 echo "seed 462 data making finished"
-sleep 1m
+sleep 15s
 
-python3 data_making.py --dataset "$DATASET" --seed 583 --test_ratio 0.1 --random_walk_len 20 --item_seq_len 250
+python3 data_making.py --dataset "$DATASET" --seed 583 --test_ratio 0.1 --random_walk_len "$RANDOM_WALK_LEN" --item_seq_len "$ITEM_SEQ_LEN"
 
 echo "seed 583 data making finished"
-sleep 1m
+sleep 15s
 
-python3 data_making.py --dataset "$DATASET" --seed 731 --test_ratio 0.1 --random_walk_len 20 --item_seq_len 250
+python3 data_making.py --dataset "$DATASET" --seed 731 --test_ratio 0.1 --random_walk_len "$RANDOM_WALK_LEN" --item_seq_len "$ITEM_SEQ_LEN"
 
 echo "seed 731 data making finished"
-sleep 1m
+sleep 15s
 
-python3 data_making.py --dataset "$DATASET" --seed 765 --test_ratio 0.1 --random_walk_len 20 --item_seq_len 250
+python3 data_making.py --dataset "$DATASET" --seed 765 --test_ratio 0.1 --random_walk_len "$RANDOM_WALK_LEN" --item_seq_len "$ITEM_SEQ_LEN"
 
 echo "seed 765 data making finished"
-sleep 1m
+sleep 15s
 
-python3 data_making.py --dataset "$DATASET" --seed 921 --test_ratio 0.1 --random_walk_len 20 --item_seq_len 250
+python3 data_making.py --dataset "$DATASET" --seed 921 --test_ratio 0.1 --random_walk_len "$RANDOM_WALK_LEN" --item_seq_len "$ITEM_SEQ_LEN"
 
 echo "seed 921 data making finished"
+sleep 15s
 
-python3 data_making.py --dataset "$DATASET" --seed 1234 --test_ratio 0.1 --random_walk_len 20 --item_seq_len 250
+python3 data_making.py --dataset "$DATASET" --seed 1234 --test_ratio 0.1 --random_walk_len "$RANDOM_WALK_LEN" --item_seq_len "$ITEM_SEQ_LEN"
 
 echo "seed 1234 data making finished"
-sleep 1m
+sleep 15s
 
-python3 data_making.py --dataset "$DATASET" --seed 12355 --test_ratio 0.1 --random_walk_len 20 --item_seq_len 250
+python3 data_making.py --dataset "$DATASET" --seed 12355 --test_ratio 0.1 --random_walk_len "$RANDOM_WALK_LEN" --item_seq_len "$ITEM_SEQ_LEN"
 
 echo "seed 12355 data making finished"
 
--- Transformer_modified/run_script.sh
+++ Transformer_modified/run_script.sh
@@ -1,32 +1,35 @@
 #!/usr/bin/env bash
 export DATASET="$1"
+export MODEL_NAME="$2"
+export USER_SEQ_LEN="$3"
+export ITEM_SEQ_LEN="$4"
 
-python3 main.py --dataset "$DATASET" --seed 42 --name model_compare_seed_42 --item_seq_len 250
-sleep 1m
+python3 main.py --dataset "$DATASET" --seed 42 --name model_compare_seed_42_"$MODEL_NAME" --user_seq_len "$USER_SEQ_LEN" --item_seq_len "$ITEM_SEQ_LEN"
+sleep 20s
 
-python3 main.py --dataset "$DATASET" --seed 62 --name model_compare_seed_62 --item_seq_len 250
-sleep 1m
+python3 main.py --dataset "$DATASET" --seed 62 --name model_compare_seed_62_"$MODEL_NAME" --user_seq_len "$USER_SEQ_LEN" --item_seq_len "$ITEM_SEQ_LEN"
+sleep 20s
 
-python3 main.py --dataset "$DATASET" --seed 365 --name model_compare_seed_365 --item_seq_len 250
-sleep 1m
+python3 main.py --dataset "$DATASET" --seed 365 --name model_compare_seed_365_"$MODEL_NAME" --user_seq_len "$USER_SEQ_LEN" --item_seq_len "$ITEM_SEQ_LEN"
+sleep 20s
 
-python3 main.py --dataset "$DATASET" --seed 462 --name model_compare_seed_462 --item_seq_len 250
-sleep 1m
+python3 main.py --dataset "$DATASET" --seed 462 --name model_compare_seed_462_"$MODEL_NAME" --user_seq_len "$USER_SEQ_LEN" --item_seq_len "$ITEM_SEQ_LEN"
+sleep 20s
 
-python3 main.py --dataset "$DATASET" --seed 583 --name model_compare_seed_583 --item_seq_len 250
-sleep 1m
+python3 main.py --dataset "$DATASET" --seed 583 --name model_compare_seed_583_"$MODEL_NAME" --user_seq_len "$USER_SEQ_LEN" --item_seq_len "$ITEM_SEQ_LEN"
+sleep 20s
 
-python3 main.py --dataset "$DATASET" --seed 731 --name model_compare_seed_731 --item_seq_len 250
-sleep 1m
+python3 main.py --dataset "$DATASET" --seed 731 --name model_compare_seed_731_"$MODEL_NAME" --user_seq_len "$USER_SEQ_LEN" --item_seq_len "$ITEM_SEQ_LEN"
+sleep 20s
 
-python3 main.py --dataset "$DATASET" --seed 765 --name model_compare_seed_765 --item_seq_len 250
-sleep 1m
+python3 main.py --dataset "$DATASET" --seed 765 --name model_compare_seed_765_"$MODEL_NAME" --user_seq_len "$USER_SEQ_LEN" --item_seq_len "$ITEM_SEQ_LEN"
+sleep 20s
 
-python3 main.py --dataset "$DATASET" --seed 921 --name model_compare_seed_921 --item_seq_len 250
-sleep 1m
+python3 main.py --dataset "$DATASET" --seed 921 --name model_compare_seed_921_"$MODEL_NAME" --user_seq_len "$USER_SEQ_LEN" --item_seq_len "$ITEM_SEQ_LEN"
+sleep 20s
 
-python3 main.py --dataset "$DATASET" --seed 1234 --name model_compare_seed_1234 --item_seq_len 250
-sleep 1m
+python3 main.py --dataset "$DATASET" --seed 1234 --name model_compare_seed_1234_"$MODEL_NAME" --user_seq_len "$USER_SEQ_LEN" --item_seq_len "$ITEM_SEQ_LEN"
+sleep 20s
 
-python3 main.py --dataset "$DATASET" --seed 12355 --name model_compare_seed_12355 --item_seq_len 250
-sleep 1m
+python3 main.py --dataset "$DATASET" --seed 12355 --name model_compare_seed_12355_"$MODEL_NAME" --user_seq_len "$USER_SEQ_LEN" --item_seq_len "$ITEM_SEQ_LEN"
+sleep 20s
