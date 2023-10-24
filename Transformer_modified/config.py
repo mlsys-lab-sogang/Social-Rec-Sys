64364723005b91bf454c5c9270639b23f25241d1
CharlieKim <skdbsxir@naver.com>
2023-10-24 19:56:02 +0900

    config: fixed ciao configuration and also added max_spd_value for making SPD embedding table.

--- Transformer_modified/config.py
+++ Transformer_modified/config.py
@@ -7,10 +7,11 @@ ciao={
             "test":8428,
          },
          "model":{
-            "num_user": 7317,
+            "num_user": 7317,       # rating(7375)에서 social에 없는 사용자 제외 -> 7317
             "max_degree_user": 804,
-            "num_item": 105114,
-            "max_degree_item": 915,
+            "num_item": 104975,     # 105114 -> id re-indexing 하고 나서 count(rating.csv로 확인) -> 104975
+            "max_degree_item": 969, # 915 -> id re-indexing 하고 나서 rating.csv로 다시 확인하니 969임. -> 969 뭘 확인한걸까.
+            # "max_spd_value": 15,    # (from _test16.py) unreachable distance를 `max(num_user) + 1` 가 아닌 실제 SPD값 보다 조금 더 큰 값으로 설정. (ciao의 spd: 1 ~ 10)
             "d_model": 64,          # MHA dim (Linear modules in Attention Network) & Embedding dim 
             "d_ffn": 256,            # FFN dim
             "num_heads": 2,
@@ -21,7 +22,7 @@ ciao={
          "training":{
             "batch_size":128,        # total_train_step: 835 (1 epoch 당 `len(train_dataset) / batch_size`)
             "optimizer":"adamw",
-            "learning_rate":0.001,
+            "learning_rate":0.0001,
             "warmup":80, 
             "lr_decay":"linear",
             "weight_decay":0,
@@ -43,6 +44,7 @@ epinions={
             "max_degree_user": 2026,
             "num_item": 261679,
             "max_degree_item": 1440,
+            # "max_spd_value": 15,    # (from _test16.py) unreachable distance를 `max(num_user) + 1` 가 아닌 실제 SPD값 보다 조금 더 큰 값으로 설정. (epinions의 spd: 1~9)
             "d_model": 64,          # MHA dim (Linear modules in Attention Network) & Embedding dim 
             "d_ffn": 256,            # FFN dim
             "num_heads": 2,
